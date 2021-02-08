afd4bb2de97b9d82cb83577dba029e8f3e88c518
cheese bae <melissaeliza0@gmail.com>
2021-02-08 17:10:40 +0900

    Initial Commit

---
+++ LocalizedNumberField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizedNumberField
+//
+//  Created by 배지영 on 2021/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalizedNumberField/LocalizedNumberFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedNumberFieldApp.swift
+//  LocalizedNumberField
+//
+//  Created by 배지영 on 2021/02/08.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedNumberFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
