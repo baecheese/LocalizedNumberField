827eb7da7847978e6253096b9cd442d9b2ebb44a
cheese bae <melissaeliza0@gmail.com>
2021-02-10 14:30:39 +0900

    locale extension : ko, en 추가

---
+++ LocalizedNumberField/Locale.swift
@@ -0,0 +1,20 @@
+//
+//  Locale.swift
+//  LocalizedNumberField
+//
+//  Created by 배지영 on 2021/02/10.
+//
+
+import Foundation
+
+extension Locale {
+    
+    static var en_US: Locale {
+        return Locale(identifier: "en_US")
+    }
+    
+    static var ko_KR: Locale {
+        return Locale(identifier: "ko_KR")
+    }
+    
+}
